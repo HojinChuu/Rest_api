999304fa50e414b0b1bd33d6049c0db80e9cbe79
HojinChuu <cnghwls7@gmail.com>
2020-08-08 00:45:17 +0900

    paginte, create

--- Controller/task.php
+++ Controller/task.php
@@ -21,6 +21,8 @@ try {
 if (array_key_exists("taskid", $_GET)) {
     $taskId = $_GET['taskid'];
 
+    var_dump($taskId);
+    die();
     if ($taskId == '' || !is_numeric($taskId)) {
         $response = new Response();
         $response->setHttpStatusCode(400);
@@ -209,6 +211,110 @@ else if (array_key_exists("completed", $_GET)) {
     }
 }
 
+// URL request page : paginate
+else if (array_key_exists("page", $_GET)) {
+
+    // GET METHOD
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $page = $_GET['page'];
+
+        if ($page == '' || !is_numeric($page)) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("Page number can not be balank and must be number");
+            $response->send();
+            exit();
+        }
+
+        $limitPerPage = 20;
+
+        try {
+            $sql = 'SELECT count(id) as totalCount FROM tbltasks';
+            $query = $readDB->prepare($sql);
+            $query->execute();
+
+            $row = $query->fetch();
+
+            $tasksCount = intval($row->totalCount);
+            $numOfPages = ceil($tasksCount / $limitPerPage);
+
+            if ($numOfPages == 0) {
+                $numOfPages = 1;
+            }
+
+            if ($page > $numOfPages || $page == 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage("Page not found");
+                $response->send();
+                exit();
+            }
+
+            $offset = ($page == 1 ? 0 : ($limitPerPage * ($page - 1)));
+
+            $sql = 'SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") AS deadline, completed 
+                    FROM tbltasks 
+                    LIMIT :pageLimit offset :offset';           
+            $query = $readDB->prepare($sql);
+            $query->bindParam(':pageLimit', $limitPerPage, PDO::PARAM_INT);
+            $query->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            $taskArray = array();
+
+            while($row = $query->fetch()) {
+                $task = new Task($row->id, $row->title, $row->description, $row->deadline, $row->completed);
+                $taskArray[] = $task->returnTaskArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['total_rows'] = $tasksCount;
+            $returnData['total_pages'] = $numOfPages;
+            $page < $numOfPages ? $returnData['has_next_page'] = true : $returnData['has_next_page'] = false;
+            $page > 1 ? $returnData['has_previous_page'] = true : $returnData['has_previous_page'] = false;
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit();
+
+        } catch (TaskException $e) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($e->getMessage());
+            $response->send();
+            exit();
+        } catch (PDOException $e) {
+            error_log("DB query error : {$e}", 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to get Tasks");
+            $response->send();
+            exit();
+        }
+    } 
+    
+    // REQUEST
+    else {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage("Request method not allowed");
+        $response->send();
+        exit();
+    }
+}
+
 // URL request none : ALL tasks
 else if (empty($_GET)) {
 
@@ -257,9 +363,119 @@ else if (empty($_GET)) {
         }
     } 
     
-    // POST REQUEST
+    // POST REQUEST : create
     else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        
+        try {
+            if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                $response->addMessage("Content type header is not set to JSON");
+                $response->send();
+                exit();
+            }
+
+            $postData = file_get_contents('php://input');
+
+            if(($jsonData = json_decode($postData)) === false) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                $response->addMessage("Request body is not valid json");
+                $response->send();
+                exit();
+            }
+
+            if (!isset($jsonData->title) || !isset($jsonData->completed)) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                !isset($jsonData->title) ? $response->addMessage("must be title") : false;
+                !isset($jsonData->completed) ? $response->addMessage("must be completed") : false;
+                $response->send();
+                exit();
+            }
+
+            $newTask = new Task(null, $jsonData->title, (isset($jsonData->description) ? $jsonData->description : null), (isset($jsonData->deadline) ? $jsonData->deadline : null), $jsonData->completed);
+
+            $title = $newTask->getTitle();
+            $description = $newTask->getDescription();
+            $deadline = $newTask->getDeadline();
+            $completed = $newTask->getCompleted();
+
+            $sql = 'INSERT INTO tbltasks (title, description, deadline, completed)
+                    VALUES (:title, :description, STR_TO_DATE(:deadline, \'%d/%m/%Y %H:%i\'), :completed)';
+            $query = $readDB->prepare($sql);
+            $query->bindParam(':title', $title, PDO::PARAM_STR);
+            $query->bindParam(':description', $description, PDO::PARAM_STR);
+            $query->bindParam(':deadline', $deadline, PDO::PARAM_STR);
+            $query->bindParam(':completed', $completed, PDO::PARAM_STR);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Failed to create Task");
+                $response->send();
+                exit();
+            }
+
+            $lastTaskID = $writeDB->lastInsertId();
+
+            $sql = 'SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") AS deadline, completed 
+                    FROM tbltasks 
+                    WHERE id = :taskid';
+            $query = $writeDB->prepare($sql);
+            $query->bindParam(':taskid', $lastTaskID, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Failed to retrieve task after create");
+                $response->send();
+                exit();
+            }
+
+            $taskArray = array();
+
+            while($row = $query->fetch()) {
+                $task = new Task($row->id, $row->title, $row->description, $row->deadline, $row->completed);
+                $taskArray[] = $task->returnTaskArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['tasks'] = $taskArray;
+            
+            $response = new Response();
+            $response->setHttpStatusCode(201);
+            $response->setSuccess(true);
+            $response->addMessage("Task created");
+            $response->setData($returnData);
+            $response->send();
+            exit();
+
+        } catch (TaskException $e) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage($e->getMessage());
+            $response->send();
+            exit();
+        } catch (PDOException $e) {
+            error_log("DB query error : {$e}", 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to insert Tasks");
+            $response->send();
+            exit();
+        }
     } 
 
     // REQUEST
